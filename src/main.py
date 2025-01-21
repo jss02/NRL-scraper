2e0bb295f011600351c3b5a6914e25503b0da20d
jss02 <jjjjjssss0326@gmail.com>
2025-01-22 01:06:33 +1100

    main.py now also produces a raw JSON file of the unparsed data

--- src/main.py
+++ src/main.py
@@ -5,11 +5,15 @@ import json
 def main(url):
     # Get match data JSON object and parse
     match_data_json = scrape_url(url, 'match')
+
+    # Write raw JSON object as JSON file
+    with open('rawoutput.json', 'w') as f2:
+        json.dump(match_data_json, f2, indent=4)
     match_data = parse_match_data(match_data_json['match'])
 
     # Write to test file as JSON file
     with open('output.json', 'w') as f:
         json.dump(match_data, f, indent=4)
-
+    
 if __name__ == '__main__':
-    main("https://www.nrl.com/draw/nrl-premiership/2024/grand-final/storm-v-panthers/")
+    main("https://www.nrl.com/draw/nrl-premiership/2002/round-3/rabbitohs-v-eagles/")
