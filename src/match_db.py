ab6950c4d2e6a2733481a01eff86bb902dbe672f
jss02 <jjjjjssss0326@gmail.com>
2025-01-26 02:27:18 +1100

    Added psql config  file, psql db connection function, and corresponding gitignore changes

--- src/match_db.py
+++ src/match_db.py
@@ -1,2 +1,22 @@
+import psycopg2
+from psycopg2 import sql
+from psql_config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+
+
+def db_conn():
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            post=DB_PORT
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to PostgreSQL database: {e}")
+        return None
+
 def save_to_db(match_data, player_stat_metadata):
+    
     pass
